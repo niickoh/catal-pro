8d05dbb4caca523f94a8594834b5b6f612da1de9
niickoh <ni.catalmir@gmail.com>
2024-03-10 19:29:28 -0300

    form contacto

--- catal-pro/src/app/components/home/home.component.ts
+++ catal-pro/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  formContacto!:FormGroup;
+
+  constructor (
+    private formBuilder: FormBuilder
+  ) { 
+    this.formContacto = this.formBuilder.group({
+      nombreCompleto: [null],
+      email: [null],
+      mensaje: [null]
+    });
+  }
+
+  enviarMensaje() {
+    console.log('datos formulario', this.formContacto);
+    this.formContacto.reset();
+  }
 
 }
